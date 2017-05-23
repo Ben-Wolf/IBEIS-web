86bbc9e256769e2ed2dae057ae432f59c3c3bbb9
Benjamin Wolf <bwolfgns@gmail.com>
2017-05-23 11:27:52 -0400

    Began reformatting for migration

--- public/app/ibeis.js
+++ public/app/ibeis.js
@@ -1,33 +1,34 @@
 angular.module('ibeis', [
-        // routes
-        'ibeis.routes',
-        // controllers
-        'workspace',
-        // services
-        'wildbook.service',
-        // imports
-        'ngMaterial', // angular material
-        'ngMessages',
-        'smart-table',
-        'ui-leaflet',
-        'angularLazyImg',
-        'infinite-scroll',
-        'lfNgMdFileInput',
+  // routes
+  'ibeis.routes',
+  // controllers
+  'workspace',
+  // services
+  'wildbook.service',
+  // imports
+  'ngMaterial', // angular material
+  'ngMessages',
+  'smart-table',
+  'ui-leaflet',
+  'angularLazyImg',
+  'infinite-scroll',
+  'lfNgMdFileInput',
 
-    ])
-    // setup theme
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            // Available palettes: red, pink, purple, deep-purple,
-            //   indigo, blue, light-blue, cyan, teal, green, light-green,
-            //   lime, yellow, amber, orange, deep-orange, brown, grey,
-            //   blue-grey
-            .primaryPalette('green')
-            .accentPalette('blue');
-    })
-    // setup utility functions
-    .run(function($rootScope) {
-        $rootScope.Utils = {
-            keys: Object.keys
-        };
-    });
+])
+
+// setup theme
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+      // Available palettes: red, pink, purple, deep-purple,
+      //   indigo, blue, light-blue, cyan, teal, green, light-green,
+      //   lime, yellow, amber, orange, deep-orange, brown, grey,
+      //   blue-grey
+    .primaryPalette('green')
+    .accentPalette('blue');
+})
+// setup utility functions
+.run(function($rootScope) {
+  $rootScope.Utils = {
+    keys: Object.keys
+  };
+});
